13b7d341637f47cf02c16d9cd6a8d4b954b73f7a
Glicu <maciek.glica@gmail.com>
2023-07-09 12:03:01 +0200

    12:02 09.07

--- tests/functional/test_acceptance.py
+++ tests/functional/test_acceptance.py
@@ -1,4 +1,4 @@
-from pytest_bdd import scenario, given, parsers, when
+from pytest_bdd import scenario, given, parsers, when, then
 
 from src import contacts
 
@@ -10,7 +10,6 @@ class TestAddingEntries:
 
         assert app._contacts == [("Jan Kowalski", "987654321")]
 
-
     def test_surnames_with_spaces(self):
         app = contacts.Application()
         app.run("contacts add Jan Kowalski-Kowalski 123456789")
@@ -21,19 +20,16 @@ class TestAddingEntries:
             ("Ludwik Mariusz Kowalski", "123456789")
         ]
 
-
     def test_invalid_strings(self):
         app = contacts.Application()
         app.run("contacts add Jan Kowalski asdfasdfsasdf")
         assert app._contacts == []
-
 
     def test_international_numbers(self):
         app = contacts.Application()
         app.run("contacts add Yennefer z Wanderbergu +48987654321")
 
         assert app._contacts == [("Yennefer z Wanderbergu", "+48987654321")]
-
 
     def test_reload(self):
         app = contacts.Application()
@@ -44,20 +40,25 @@ class TestAddingEntries:
         app.load()
         assert app._contacts == [("Yennefer z Wanderbergu", "+48987654321")]
 
-    @given("mam książkę adresową", target_fixture="contactbook")
-    def contactbook(self):
-        return contacts.Application()
-
-
-    @given(parsers.parse("Mam wpis dotyczący użytkownika \"{contactname}\""))
-    def have_a_contact(self, contactbook, contactname):
-        contactbook.add(contactname, "123456789")
-
-    @when(parsers.parse("Po wydaniu polecenia \"{command}\""))
-    def runcommand(self, contactbook, command):
-        contactbook.run(command)
-
-    @scenario(r"C:\Users\Glicu\PycharmProjects\tester0907\tests\acceptance\delete_contact.feature",
+@scenario(r"C:\Users\Glicu\PycharmProjects\tester0907\tests\acceptance\delete_contact.feature",
               "Usunięcie wpisu z książki adresowej")
-    def test_deleting(self):
-        pass
+def test_deleting_contacts():
+    pass
+
+@given("mam książkę adresową", target_fixture="contactbook")
+def contactbook():
+    return contacts.Application()
+
+@given(parsers.parse("Mam wpis dotyczący użytkownika \"{contactname}\""))
+def have_a_contact(contactbook, contactname):
+    contactbook.add(contactname, "123456789")
+
+@when(parsers.parse("Po wydaniu polecenia \"{command}\""))
+def runcommand(contactbook, command):
+    contactbook.run(command)
+
+@then("wpis nie będzie już widoczny w książce adresowej")
+def emptylist(contactbook):
+    assert contactbook._contacts == []
+
+
